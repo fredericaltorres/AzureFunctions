89646b2db2683ac079435a496fb5d33cfe869979
Frederic Torres <ftorres@emoneyadvisor.com>
2018-09-09 18:26:40 -0400

    Update powershell unit tests

--- AzureFunctions/Testing/curl.ps1
+++ AzureFunctions/Testing/curl.ps1
@@ -5,20 +5,25 @@ cls
 
 $BaseUrl = "http://localhost:7071/api"
 
-$TestResultUrl = "$BaseUrl/test/reset"
-$result = apiGet $TestResultUrl
+$url = "$BaseUrl/test/reset"
+$result = apiGet $url
 
 Assert-Verbose $true
-Assert-AreEqual $true $result
+Assert-AreEqual $true $result | Out-Null
 
-$TestResultUrl = "$BaseUrl/todo"
+$url = "$BaseUrl/todo"
 $result = apiGet $url
-Assert-AreEqual 0 $result.Length
+Assert-AreEqual 0 $result.Length | Out-Null
 
-$desc = "Hello $([System.Environment]::TickCount)"
-$json = '{TaskDescription:"'+$desc+'"}'
-$result = apiPost $url $json
-"New todo created $($result.id)"
+function createNewTodo() {
+    $desc = "Hello $([System.Environment]::TickCount)"
+    $json = '{TaskDescription:"'+$desc+'"}'
+    $result = apiPost $url $json
+    "New todo created $($result.id)"
+    return $desc
+}
+$desc = createNewTodo
 
 $result = apiGet $url
-Assert-AreEqual 1 $result.Length "Verify array length"
+Assert-AreEqual 1 $result.Length "Verify array length" | Out-Null
+Assert-AreEqual $desc $result[0].TaskDescription
